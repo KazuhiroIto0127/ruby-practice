5f7ace3c4fadee972d3e38244b1f67ddc3ffa963
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2024-03-23 23:03:52 +0900

    rubyのバージョンを3.2.3に変更。hello.rbを追加。

--- hello/hello.rb
+++ hello/hello.rb
@@ -1,7 +1,26 @@
-p 'hello world'
+def hello
+  p 'hello'
+end
+
+hello
 
 i = 0
 
 str = "test"
 
 p i,str
+
+class Person
+          def initialize(age, name)
+      @age = age
+      @name = name
+    end
+
+    def hello
+      p "my name is #{@name}. age is #{@age}"
+    end
+end
+
+
+p = Person.new(10, '吉田')
+p.hello
